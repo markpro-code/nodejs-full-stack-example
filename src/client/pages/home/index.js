0027a6db7b1035be1e22ddedbeb4d1b9a081b6aa
markpro-code <markpro.code@gmail.com>
2019-10-14 10:08:41 +0800

    update

--- src/client/pages/home/index.js
+++ src/client/pages/home/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
 
-import style from './index.less'
-
 export default class PageHome extends React.Component {
     /* --------- [handlers-start] ----------- */
     /* --------- [handlers-end] ----------- */
 
     render() {
-        return (
-            <div className={style.page}>
-            <h3>Login</h3>
-            </div>
-        )
+        return (<h1> Home </h1>)
     }
 }
--- src/client/pages/main/index.js
+++ src/client/pages/main/index.js
@@ -1,8 +1,12 @@
 import { hot } from 'react-hot-loader/root'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import React from 'react'
 import { connect } from 'react-redux'
 import { createSelector } from 'reselect'
 import { withRouter } from 'react-router'
+
+import PageHome from '@/pages/home'
+import PageLogin from '@/pages/login'
 
 import style from './index.less'
 
@@ -13,9 +17,22 @@ class Main extends React.Component {
     }
 
     render() {
+        const showHeader = this.props.location.pathname !== '/login'
+
         return (
             <div className={style.main_page}>
-                <h1>Main Page</h1>
+                {!showHeader ? null : (
+                    <div className={style.header}>
+                        <h3>header</h3>
+                    </div>
+                )}
+                <div className={style.main_content}>
+                    <Switch>
+                        <Redirect exact={true} from="/" to="/home" />
+                        <Route path="/home" component={PageHome} />
+                        <Route path="/login" component={PageLogin} />
+                    </Switch>
+                </div>
             </div>
         )
     }
